89e2f985b2a7bc08bce480cbdeec554d3d4b3edb
ShogherS <shogher@outlook.com>
2023-09-24 23:28:07 +0400

    third commit

--- init.h
+++ init.h
@@ -9,45 +9,302 @@
 
 struct Init
 {
-	Init(Varaiables& v) : vars{v} {}
-	
-	std::map<std::string, std::functional<void(std::vector<std::string>&)>> operations {
-		 {"=", [](std::vector<std::string>& line, std::string type) {
-				size_t i = 2;
-				if (type == "bool") {
-					bool& value = variables.booleans[0].first;
-					if (line[i] == "true") {
-						value = true;
-					} else if ( line[i] == "false") {
-						value == false;
-					} else if (variables.booleans.find(line[i]) != variables.booleans.end()) {
-						value = variables.booleans.[line[i]].first;
-					} else {
-						  throw std::invalid_argument("Invalid argument \"" + line[i] + "\".");    
-					}                                             
+		std::map<std::string, std::function<void(std::vector<std::string>&, std::string&, Variables&)>> operations {
+		 {"=", [](std::vector<std::string>& line, std::string& type, Variables& variables) {
+			size_t i = 2;
+			if (type == "bool") {
+				bool& value = variables.booleans[line[0]].first;
+				if (line[i] == "true") {
+					value = true;
+				} else if ( line[i] == "false") {
+					value = false;
+				} else if (variables.booleans.find(line[i]) != variables.booleans.end()) {
+					value = variables.booleans[line[i]].first;
+				} else {
+					  throw std::invalid_argument("Invalid argument \"" + line[i] + "\".");    
+				}                                             
+			} else if (type == "int") {
+				int& value = variables.integers[line[0]].first;
+				if (variables.integers.find(line[i]) != variables.integers.end()) {
+					value = variables.integers[line[i]].first;
+				} else {
+					value = std::stoi(line[i]);
+				}	
+			} else if (type == "char") {
+				char& value = variables.characters[line[0]].first;
+				if (variables.characters.find(line[i]) != variables.characters.end()) {
+					value = variables.characters[line[i]].first;
+				} else if (line[i].size() == 3 && line[i][0] == '\'' && line[i][2] == '\'' ) {
+					value = line[i][1];
+				} else {
+					  throw std::invalid_argument("Invalid argument \"" + line[i] + "\".");    
+				}                                             
+			} else if (type == "string") {
+				std::string& value = variables.strings[line[0]].first;
+				if (variables.strings.find(line[i]) != variables.strings.end()) {
+					value = variables.strings[line[i]].first;
+				} else {
 					++i;
-					if (line[i] !== ";") {
-						throw std::invalid_argument("Unknown symbole \"" + line[i]+ "\".");                         
+					while (i < line.size() &&  line[2].back() != '\"') {
+						line[2] += " " + line[i];
+						++i;
 					}
-				} else if (type == "int") {
-				
-				} else if (type == "char") {
-				
-				} else if (type == "string") {
-				
+					if (line[2].front() !='\"' || line[2].back() != '\"') {
+						throw std::invalid_argument("Invalid value. A stirng literal is excpectrd.");	
+					}		
+					value = line[2];
 				}
-
-		}  }
-	/*
-	{"+=", },
-	{"-=", },
-	{"++", },
-	{"--", },
-	{"==", },
-	{"<", },
-	{}
-	*/
+			}
+			++i;
+			if (i >= line.size() || line[i] != ";") {
+					throw std::invalid_argument("Unknown symbole \"" + line[i]+ "\".");                         
+			}
+	}},
+	{"+=", [](std::vector<std::string>& line, std::string& type, Variables& variables) {
+		size_t i = 2;
+		if (type == "bool") {
+			throw std::invalid_argument("Invalid operation.  += for type bool.");                          
+		} else if (type == "int") {
+			int& value = variables.integers[line[0]].first;
+			if (variables.integers.find(line[i]) != variables.integers.end()) {
+				value += variables.integers[line[i]].first;
+			} else {
+				value += std::stoi(line[i]);
+			}	
+		} else if (type == "char") {
+			throw std::invalid_argument("Invalid operation.  += for type char.");                          
+		} else if (type == "string") {
+			std::string& value = variables.strings[line[0]].first;
+			if (variables.strings.find(line[i]) != variables.strings.end()) {
+				value += variables.strings[line[i]].first;
+			} else {
+				if (line[2].front() !='\"' || line[2].back() != '\"') {
+					throw std::invalid_argument("Invalid value. A stirng literal is excpectrd.");	
+				}		
+			}
+			value += line[2];
+		}
+		++i;
+		if (i >= line.size() || line[i] != ";") {
+				throw std::invalid_argument("Unknown symbole \"" + line[i]+ "\".");                         
+		}
+	}},
+	{"-=", [](std::vector<std::string>& line, std::string& type, Variables& variables) {
+		size_t i = 2;
+		if (type == "bool") {
+			throw std::invalid_argument("Invalid operation.  -= for type bool.");                          
+		} else if (type == "int") {
+			int& value = variables.integers[line[0]].first;
+			if (variables.integers.find(line[i]) != variables.integers.end()) {
+				value -= variables.integers[line[i]].first;
+			} else {
+				value -= std::stoi(line[i]);
+			}	
+		} else if (type == "char") {
+			throw std::invalid_argument("Invalid operation.  -= for type char.");                          
+		} else if (type == "string") {
+			throw std::invalid_argument("Invalid operation.  -= for type string.");                          
+		} else if (type == "string") {
+		}
+		++i;
+		if (i >= line.size() || line[i] != ";") {
+				throw std::invalid_argument("Unknown symbole \"" + line[i]+ "\".");                         
+		}
+	}},
+	{"++", [](std::vector<std::string>& line, std::string& type, Variables& variables){
+		if (type == "bool") {
+			throw std::invalid_argument("Invalid operation.  -- for type bool.");                          
+		} else if (type == "int") {
+			int& value = variables.integers[line[0]].first;
+			++value;	
+		} else if (type == "char") {
+			char& value = variables.characters[line[0]].first;
+			++value;	
+		} else if (type == "string") {
+			throw std::invalid_argument("Invalid operation.  -= for type string.");                          
+		}
+		if ( 2 >= line.size() || line[2] != ";") {
+			throw std::invalid_argument("Unknown symbole \"" + line[2]+ "\".");                         
+		}
+	} },
+	{"--", [](std::vector<std::string>& line, std::string& type, Variables& variables){
+		if (type == "bool") {
+			throw std::invalid_argument("Invalid operation.  -- for type bool.");                          
+		} else if (type == "int") {
+			int& value = variables.integers[line[0]].first;
+			--value;	
+		} else if (type == "char") {
+			char& value = variables.characters[line[0]].first;
+			--value;	
+		} else if (type == "string") {
+			throw std::invalid_argument("Invalid operation.  -= for type string.");                          
+		}
+		if ( 2 >= line.size() || line[2] != ";") {
+			throw std::invalid_argument("Unknown symbole \"" + line[2]+ "\".");                         
+		}
+	} }
 	};
-	Variables& vars;
+	std::map<std::string, std::function<bool(std::string&, std::string&, Variables&)>> cmp {
+	{"==", [this](std::string& argument1, std::string& argument2, Variables& variables) -> bool {
+	// while (a < b) {       while (a < 20) {   while (40 > 34) {			
+		std::string type = isDeclared(variables, argument1);
+		std::string type2 = isDeclared(variables, argument2);
+		if (type2 != "NO") {
+			std::string tmp = argument2;
+			argument2 = argument1;
+			argument1 = tmp;
+		}
+		if (type == "bool") {
+			bool value = variables.booleans[argument1].first;
+			if (argument2 == "true") {
+				return value == true ? true : false;
+			} else if ( argument2 == "false") {
+				return value == false ? true : false;
+			} else if (type2 == "bool") {
+				return value == variables.booleans[argument2].first ? true : false;
+			} else {
+				  throw std::invalid_argument("Invalid argument \"" + argument2  + "\".");    
+			}                                             
+		} else if (type == "int") {
+			int& value = variables.integers[argument1].first;
+			if (type2 == "int") {
+				return value == variables.integers[argument2].first ? true : false;
+			} else {
+				return value == std::stoi(argument2) ? true : false;
+			}	
+		} else if (type == "char") {
+			char& value = variables.characters[argument1].first;
+			if (type2 == "char") {
+				return value == variables.characters[argument2].first ? true : false;
+			} else if (argument2.size() == 3 && argument2[0] == '\'' && argument2[2] == '\'' ) {
+				return value == argument2[1]? true : false;
+			} else {
+				  throw std::invalid_argument("Invalid argument \"" + argument2 + "\".");    
+			}                                             
+		} else if (type == "string") {
+			std::string& value = variables.strings[argument1].first;
+			if (type2 == "string") {
+				return value == variables.strings[argument2].first ? true : false;
+			} else {
+				if (argument2.front() !='\"' || argument2.back() != '\"') {
+					throw std::invalid_argument("Invalid value " + argument2 + ". A stirng literal is excpectrd.");	
+				}		
+			return value == argument2 ? true : false;
+			}
+		}
+		throw std::invalid_argument("THF ==");
+		return false;
+	} },
+	{"<", [this](std::string& argument1, std::string& argument2, Variables& variables) -> bool {
+		std::string type = isDeclared(variables, argument1);
+		std::string type2 = isDeclared(variables, argument2);
+/*
+		if (type2 != "NO") {
+			std::string tmp = argument2;
+			argument2 = argument1;
+			argument1 = tmp;
+		}
+*/		if (type == "bool") {
+			throw std::invalid_argument("Invalid operation for boolean type.");
+		} else if (type == "int") {
+			int value = variables.integers[argument1].first;
+			if (type2 == "int") {
+				std::cout << value << " < " << variables.integers[argument2].first << std::endl;
+				return value < variables.integers[argument2].first ? true : false;
+			} else {
+				return value < std::stoi(argument2) ? true : false;
+			}	
+		} else if (type == "char") {
+			char value = variables.characters[argument1].first;
+			if (type2 == "char") {
+				return value < variables.characters[argument2].first ? true : false;
+			} else if (argument2.size() == 3 && argument2[0] == '\'' && argument2[2] == '\'' ) {
+				return value < argument2[1]? true : false;
+			} else {
+				  throw std::invalid_argument("Invalid argument \"" + argument2 + "\".");    
+			}                                             
+		} else if (type == "string") {
+			throw std::invalid_argument("Invalid operation for string type.");	
+		}
+		throw std::invalid_argument("THF <");
+		return false;
+	} },
+	{">", [this](std::string& argument1, std::string& argument2, Variables& variables) -> bool {
+		
+		return false;
+	} } 
+	};
+
+	std::map<std::string, std::function<bool(std::vector<std::vector<std::string>>&, Variables&,  size_t&)>> ifwh {
+		{"if", [this](std::vector<std::vector<std::string>>& code, Variables& variables, size_t& line) -> bool {
+			// if (a > b) {
+			if (code[line].size() < 5) {	
+				throw std::invalid_argument("Some arguments arre missing in line " + std::to_string(line) + ".");
+			}
+			if (!code[line][1].starts_with('(')) {
+				throw std::invalid_argument("A ( is expectred in line " + std::to_string(line) + ".");
+			}
+			if (!cmp.contains(code[line][2])) {		
+				throw std::invalid_argument("An invalid operator in line " + std::to_string(line) + ".");
+			}
+			if (!code[line][3].ends_with(')')){
+				throw std::invalid_argument("A ) id expected in line" + std::to_string(line) + ".");
+			}
+			std::string operand1 = code[line][1].substr(1);
+			std::string operation = code[line][2];
+			std::string operand2 = code[line][3];
+			operand2.pop_back(); 
+			bool flag =  cmp[operation](operand1, operand2, variables);
+			if (!flag) {
+				while (code[line][0] != "}") {
+					++line;
+					std::cout << "the line is skiped " << line << std::endl;
+				}
+			}
+			return false;
+		} },
+		{"while", [this](std::vector<std::vector<std::string>>& code, Variables& variables, size_t& line) -> bool {
+			if (code[line].size() < 5) {	
+				throw std::invalid_argument("Some arguments arre missing in line " + std::to_string(line) + ".");
+			}
+			if (!code[line][1].starts_with('(')) {
+				throw std::invalid_argument("A ( is expectred in line " + std::to_string(line) + ".");
+			}
+			if (!cmp.contains(code[line][2])) {		
+				throw std::invalid_argument("An invalid operator in line " + std::to_string(line) + ".");
+			}
+			if (!code[line][3].ends_with(')')){
+				throw std::invalid_argument("A ) id expected in line" + std::to_string(line) + ".");
+			}
+			std::string operand1 = code[line][1].substr(code[line][1].size() - 1);
+			std::string operation = code[line][2];
+			std::string operand2 = code[line][3];
+			operand2.pop_back(); 
+			bool flag =  cmp[operation](operand1, operand2, variables);
+			if (!flag) {
+				while (code[line][0] != "}") {
+					++line;
+					std::cout << line <<" line id skiped in while" << std::endl;
+				}
+			}
+			return flag;
+	} }
+	};
+	std::string isDeclared(Variables&, std::string&);
 };
+
+std::string Init::isDeclared(Variables& variables, std::string& key)
+{
+	if (variables.integers.find(key) != variables.integers.end()) {
+		return variables.integers[key].second;
+	} else if (variables.booleans.find(key) != variables.booleans.end()) {
+		return  variables.booleans[key].second;	
+	} else if (variables.characters.find(key) != variables.characters.end()) {
+		return variables.characters[key].second;
+	} else if (variables.strings.find(key) != variables.strings.end()) {	
+		return variables.strings[key].second;
+	}
+	return "NO";
+}
+
 #endif //INIT_H
--- parser.h
+++ parser.h
@@ -4,8 +4,13 @@
 #include <string>
 #include <stdexcept>
 #include <stack>
+#include <sstream>
+#include <functional>
+
 
 #include "loader.h"
+#include "variables.h"
+#include "init.h"
 
 struct Parser
 {
@@ -13,7 +18,7 @@ struct Parser
 	bool hasMain(size_t);
 	void scopeParser(const char, const char, const size_t) const;
 	bool hasIostream();
-	bool variabelParser(size_t) const;
+	bool variableParser(size_t&);
 	bool variableTypeParser(size_t) const;
 	void commentParser(size_t);
 	void semicolonParser(size_t) const;
@@ -21,19 +26,28 @@ struct Parser
 	void pars();
 	
 	//helper
+	void processData();
 	void removeSpace(size_t);
 	void trimString(std::string&);
+	void print()const;
+	std::string isDeclared(const size_t, const size_t);
 	std::vector<std::string>& data;
-	// std::map<std::string, bool> flag
+	std::vector<std::vector<std::string>> code;
 
+//helper function to check the line
+	void checkRedefinition(const size_t line) const;	
+	void checkSemicolon(const size_t line)const;
+	//variables
+	Variables variables;
+	Init init;
 };
 
-Parser::Parser(std::vector<std::string>& d) : data{d} {}
+Parser::Parser(std::vector<std::string>& d) : data{d} { code.resize(data.size() - 2); }
 
 void Parser::pars()
 {
 	size_t i = hasIostream() ? 1 : 0;
-	std::cout << data[i] << std::endl;
+	size_t ioFlag = i;
 	hasMain(i);
 	++i;	
 	for ( ; i < data.size() - 1; ++i) {
@@ -43,25 +57,119 @@ void Parser::pars()
 		scopeParser('[', ']', i);
 		semicolonParser(i);
 	}
-
-
+	parsCurlyBraces();
+	processData();
+	for (size_t i{}; i < code.size() - ioFlag; ++i) {
+		variableParser(i);
+	}
+	print();
 }
 
 bool Parser::hasMain(size_t mainLine)
 {
 	removeSpace(mainLine);
-	std::cout << data[mainLine] << std::endl; 
 	if (data[mainLine] != "intmain(){") {
 		throw std::invalid_argument("The program does not have a main.");
 	} else {
 		removeSpace(data.size() - 1);
 		if (data.back() != "}") {
-			std::cout << data.back() << std::endl;	
 			throw std::invalid_argument("The function main does not have a closing scope.");
 		}
 	}
 	return true;
 }
+
+bool Parser::variableParser(size_t& line)
+{
+	std::vector<std::string>& current = code[line];
+	std::cout << current[0]  << "____tins line is excecuted. Line : " << line <<std::endl;
+	if (current[1].back() == ']') {
+	} else if (current[0] == "bool") {
+		checkRedefinition(line);
+		if (current[3] != "true" && current[3] != "false") {
+			throw std::invalid_argument("Invalid value in line " + std::to_string(line) + ", true or false is excpected.");
+		}
+		bool val = current[3] == "true" ? true : false;
+		std::pair<bool, std::string> p (val, "bool");
+		variables.booleans[current[1]] = p;
+		
+		checkSemicolon(line);
+	} else if (current[0] == "char") {
+		checkRedefinition(line);	
+		if (current[3].size() != 3 || current[3][0] != '\'' || current[3][2] != '\'') {
+			throw std::invalid_argument("Invalid value in line " + std::to_string(line) + ", a single character is excpected.");
+		}
+		checkSemicolon(line);
+		char val = current[3][1];
+		std::pair<char, std::string> p (val, "char");
+		variables.characters[current[1]] = p;
+	} else if (current[0] == "int") {
+		checkRedefinition(line);
+		int value = std::stoi(current[3]);
+		checkSemicolon(line);
+		std::pair<int, std::string> p(value, "int");
+		variables.integers[current[1]] = p;		
+
+	} else if (current[0] == "string") {
+		checkRedefinition(line);
+		
+		size_t count{4};
+		while (count < current.size() &&  current[3].back() != '\"') {
+			current[3] += " " + current[count];
+			++count;
+		}
+		if (current[3].front() !='\"' || current[3].back() != '\"') {
+			throw std::invalid_argument("Invalid value in line " + std::to_string(line) + ". A stirng literal is excpectrd.");	
+	}
+		if (count != current.size() - 1 || current[count] != ";") {
+			throw std::invalid_argument("Invalid argument at line " + std::to_string(line) + ".");
+		}
+		std::pair<std::string, std::string> p (current[3], "string");
+		variables.strings[current[1]] = p;
+
+	} else if (current[0] == "float") {
+	
+	} else if (current[0] == "double") {
+	
+	} else if (init.ifwh.contains(current[0])) {
+		bool flag = init.ifwh[current[0]](code,variables, line);
+		std::cout << "The flag is : "<< flag << " In line " << line << std::endl;
+		while (flag) {
+			std::cout << "while flag is set "<< line << std::endl;
+			size_t while_line = line;
+			while (code[++line][0] != "}") {
+				std::cout << "The code is excexuted: " << line << std::endl;
+				variableParser(line);
+			}
+			line = while_line;
+			flag = init.ifwh["while"](code,variables,line);
+		}
+	} else if (current[0] == "{") {
+	
+	} else if (current[0] == "}") {
+	
+	} else if (current[0] == "return") {
+		
+	} else if (current[0] == "std::cout") {
+	
+	} else if (current[0] == "std::cin") {
+	
+	} else if (current[0] == "break"){
+	
+	} else {
+		std::string varType = isDeclared(line, 0);
+		if (varType == "NO") {	
+			throw std::invalid_argument("Use of undeclared identifier " + std::to_string(line) + ".");
+		}
+		if (init.operations.find(current[1]) != init.operations.end()) {
+			init.operations[current[1]](current, varType, variables);
+		} else {
+			throw std::invalid_argument("Unknown argument at line " + std::to_string(line) + "."); 
+		}
+	}
+	return true;
+}
+
 void Parser::scopeParser(const char open, const char close, const size_t pos) const
 {
 	size_t countOfOpen{}, countOfClose{};
@@ -91,7 +199,6 @@ void Parser::semicolonParser(const size_t pos) const
 {
 	std::string& line = data[pos];
 	char end = line.back();
-	std::cout << line << " And the last symbol is " << end << "	" << (end == ';') << std::endl;
 	if (end == ';' ||  end == '{' || end == '}' ) {
 		return;
 	}
@@ -101,18 +208,16 @@ void Parser::semicolonParser(const size_t pos) const
 void Parser::parsCurlyBraces()
 {
 	std::stack<char> braces;
-
 	for (size_t i{}; i < data.size(); ++i) {
 		for (const char c : data[i]) {
 			if (c == '{') {
 				braces.push('{');
 			} else if ( c == '}') {
-				if (braces.size() > 1) {
-					throw std::invalid_input("The } is excpected in line " + std::to_string(i) + ".");
+				if (braces.empty()) {
+					throw std::invalid_argument("The } is excpected in line " + std::to_string(i) + ".");
 				}
 				braces.pop();
 			}
-			
 		}
 	}	
 	if (!braces.empty()) {
@@ -125,10 +230,54 @@ bool Parser::hasIostream()
 	std::string& iostream = data[0];
 	removeSpace(0);
 	return data[0] == "#include<iostream>" ? true : false;
-}	
+}
+	
+void Parser::processData()
+{
+	size_t i = hasIostream() ? 2 : 1;
+	size_t j{};
+	while (i < data.size() - 1) {
+		std::istringstream lineStream(data[i]);
+		std::string current;
+			while (lineStream >> current) {
+				//std::cout << "its ok before this line" << std::endl;
+				code[j].push_back(current);
+			}
+		if (code[j].back().back() == ';') {
+			code[j].back().pop_back();
+			code[j].push_back(";");
+		}
+		// --num
+		if (code[j].size() > 1 && code[j][0].starts_with("--")) {
+			std::string name = code[j][0].substr(2);
+			code[j].insert(code[j].begin(), name);
+			code[j][1] = "--";
+		}
+		// num--
+		if (code[j].size() > 1 && code[j][0].ends_with("--")) {
+			std::string name = code[j][0].substr(0, (code[j][0].size() - 2));
+			code[j].insert(code[j].begin(), name);
+			code[j][1] = "--";
+		}
+		//--num
+		if (code[j].size() > 1 && code[j][0].ends_with("++")) {
+			std::string name = code[j][0].substr(0, (code[j][0].size() - 2));
+			code[j].insert(code[j].begin(), name);
+			code[j][1] = "++";
+		}
+		// ++num
+		if (code[j].size() > 1 && code[j][0].starts_with("++")) {
+		    std::string name = code[j][0].substr(2);
+		    code[j].insert(code[j].begin(), name);
+		    code[j][1] = "++";
+		}
+		++i;
+		++j;
+	}
+}
 
-
-void Parser::trimString(std::string& str) {
+void Parser::trimString(std::string& str) 
+{
     size_t start = str.find_first_not_of(" \t\n\r"); // Find the first non-white space character
     if (start == std::string::npos) {
         // The string consists only of white spaces
@@ -140,10 +289,77 @@ void Parser::trimString(std::string& str) {
     str = str.substr(start, end - start + 1); // Assign the trimmed part back to the string
 }
 
-
 void Parser::removeSpace(size_t pos)
 {
 	data[pos].erase(std::remove_if(data[pos].begin(), data[pos].end(), ::isspace), data[pos].end()); 
-	std::cout << data[pos] << std::endl;
 }
+
+void Parser::print()const
+{
+	std::cout << "____________CODE____________" << std::endl;
+	for (size_t i{}; i < code.size(); ++i) {
+		std::cout <<i <<": " ;
+		for (auto& word : code[i]) {
+			std::cout << word << " " ;
+		}
+		std::cout << std::endl;
+	}
+	std::cout << "_____________________________________\n______________Variagles__________________" << std::endl;
+	
+	for (auto& item : variables.booleans) {
+		std::cout << "Variable name: " << item.first << " value: " << item.second.first << " type: " << item.second.second << std::endl; 
+	}
+	for (auto& item : variables.integers) {
+		std::cout << "Variable name: " << item.first << " value: " << item.second.first << " type: " << item.second.second << std::endl; 
+	}
+	for (auto& item : variables.characters) {
+		std::cout << "Variable name: " << item.first << " value: " << item.second.first << " type: " << item.second.second << std::endl; 
+	}
+	for (auto& item : variables.strings) {
+		std::cout << "Variable name: " << item.first << " value: " << item.second.first << " type: " << item.second.second << std::endl; 
+	}
+}
+void Parser::checkSemicolon(const size_t line)const
+{
+	if (code[line][4] != ";") {
+		throw std::invalid_argument("Unknown symbole " + code[line][4] + " in line " + std::to_string(line) + ".");}
+}
+std::string Parser::isDeclared(const size_t line, const size_t pos)
+{
+	const std::string& key = code[line][pos];
+	if (variables.integers.find(key) != variables.integers.end()) {
+		return variables.integers[key].second;
+	} else if (variables.booleans.find(key) != variables.booleans.end()) {
+		return  variables.booleans[key].second;	
+	} else if (variables.characters.find(key) != variables.characters.end()) {
+		return variables.characters[key].second;
+	} else if (variables.strings.find(key) != variables.strings.end()) {	
+		return variables.strings[key].second;
+	}
+	return "NO";
+}
+void Parser::checkRedefinition(const size_t line) const	
+{
+	if (code[line][0] == "bool") {
+		if (variables.booleans.contains(code[line][1])) {
+		throw std::invalid_argument("Redefinition in line " + std::to_string(line) + " .");
+		}
+	} else if (code[line][0] == "char") {
+		if (variables.characters.contains(code[line][1])) {
+			throw std::invalid_argument("Redefinition in line " + std::to_string(line) + " .");
+		}
+	} else if (code[line][0] == "int") {
+		if (variables.integers.contains(code[line][1])) {
+			throw std::invalid_argument("Redefinition in line " + std::to_string(line) + " .");
+		}
+	} else if (code[line][0] == "string") {
+		if (variables.strings.contains(code[line][1])) {
+			throw std::invalid_argument("Redefinition in line " + std::to_string(line) + " .");
+		}
+	}
+	if (code[line][2] != "=") {
+		throw std::invalid_argument("In line " + std::to_string(line) + " = is expected");
+	}
+}
+
 #endif //PARSER_H
