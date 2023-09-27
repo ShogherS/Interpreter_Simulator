ceaab029df372bfd20c3d8898b0bd82aadc1bfa7
ShogherS <shogher@outlook.com>
2023-09-27 10:33:20 +0400

    commit

--- parser.h
+++ parser.h
@@ -34,6 +34,7 @@ struct Parser
 	std::vector<std::string>& data;
 	std::vector<std::vector<std::string>> code;
 
+	std::pair<std::string, int> arrNamePars(std::string&, Variables&);
 //helper function to check the line
 	void checkRedefinition(const size_t line) const;	
 	void checkSemicolon(const size_t line)const;
@@ -85,54 +86,74 @@ bool Parser::variableParser(size_t& line)
 	std::vector<std::string>& current = code[line];
 	std::cout << current[0]  << "____tins line is excecuted. Line : " << line <<std::endl;
 	if (current[1].back() == ']') {
+		std::pair<std::string, int> arrInfo =  arrNamePars(current[1], variables);
+		
+
+
 	} else if (current[0] == "bool") {
 		checkRedefinition(line);
-		std::string type2 = isDecleared(line, 3);
-//		if (type2 == "bool") {
-			
-
-
-		if (current[3] != "true" && current[3] != "false") {
+		std::string type2 = isDeclared(line, 3);
+		bool val{};
+		if (type2 == "bool") {
+			val = variables.booleans[current[3]].first;
+		} else if (current[3] == "true" || current[3] == "false") {
+			val = "true" ? true : false;
+		} else {
 			throw std::invalid_argument("Invalid value in line " + std::to_string(line) + ", true or false is excpected.");
 		}
-		bool val = current[3] == "true" ? true : false;
+		checkSemicolon(line);
 		std::pair<bool, std::string> p (val, "bool");
 		variables.booleans[current[1]] = p;
-		
-		checkSemicolon(line);
 	} else if (current[0] == "char") {
-		checkRedefinition(line);	
-		if (current[3].size() != 3 || current[3][0] != '\'' || current[3][2] != '\'') {
+		checkRedefinition(line);
+		std::string type2 = isDeclared(line, 3);
+		char val{};	
+		if (current[3].size() == 3 && current[3][0] == '\'' &&  current[3][2] == '\'') {
+			char val = current[3][1];
+		} else if (type2 == "char") {
+			val = variables.characters[current[3]].first;
+		} else {
 			throw std::invalid_argument("Invalid value in line " + std::to_string(line) + ", a single character is excpected.");
 		}
 		checkSemicolon(line);
-		char val = current[3][1];
 		std::pair<char, std::string> p (val, "char");
 		variables.characters[current[1]] = p;
 	} else if (current[0] == "int") {
 		checkRedefinition(line);
-		int value = std::stoi(current[3]);
+		int val{};
+		std::string type2 = isDeclared(line, 3); 
+		if (type2 == "int") {
+			val = variables.integers[current[3]].first;
+		} else {
+			val = std::stoi(current[3]);
+		}
 		checkSemicolon(line);
-		std::pair<int, std::string> p(value, "int");
+		std::pair<int, std::string> p(val, "int");
 		variables.integers[current[1]] = p;		
 
 	} else if (current[0] == "string") {
 		checkRedefinition(line);
-		
-		size_t count{4};
-		while (count < current.size() &&  current[3].back() != '\"') {
-			current[3] += " " + current[count];
-			++count;
+		std::string type2 = isDeclared(line, 3);
+		std::string val;
+		if (type2 == "string") {
+			val = variables.strings[current[3]].first;
+		} else {
+			size_t count{4};
+			val = current[3];
+			while (count < current.size() && val.back() != '\"') {
+				val +=  " " + current[count];
+				++count;
+			}
+			std::cout << val << std::endl;
+			if (val.front() !='\"' || val.back() != '\"') {
+					throw std::invalid_argument("Invalid value in line " + std::to_string(line) + ". A stirng literal is excpectrd.");	
 		}
-		if (current[3].front() !='\"' || current[3].back() != '\"') {
-			throw std::invalid_argument("Invalid value in line " + std::to_string(line) + ". A stirng literal is excpectrd.");	
-	}
-		if (count != current.size() - 1 || current[count] != ";") {
-			throw std::invalid_argument("Invalid argument at line " + std::to_string(line) + ".");
+			if (count != current.size() - 1 || current[count] != ";") {
+				throw std::invalid_argument("Invalid argument at line " + std::to_string(line) + ".");
+			}
 		}
-		std::pair<std::string, std::string> p (current[3], "string");
+		std::pair<std::string, std::string> p (val, "string");
 		variables.strings[current[1]] = p;
-
 	} else if (current[0] == "float") {
 	
 	} else if (current[0] == "double") {
@@ -373,6 +394,14 @@ std::string Parser::isDeclared(const size_t line, const size_t pos)
 		return variables.characters[key].second;
 	} else if (variables.strings.find(key) != variables.strings.end()) {	
 		return variables.strings[key].second;
+	} else if (variables.boolArr.contains(key)) {
+		return "boolArr";
+	} else if (variables.charArr.contains(key)) {
+		return "charArr";	
+	} else if (variables.intArr.contains(key)) {
+		return "intArr";
+	} else if (variables.stringArr.contains(key)) {
+		return "stringArr";
 	}
 	return "NO";
 }
@@ -398,6 +427,23 @@ void Parser::checkRedefinition(const size_t line) const
 	if (code[line][2] != "=") {
 		throw std::invalid_argument("In line " + std::to_string(line) + " = is expected");
 	}
+}
+std::pair<std::string, int> Parser::arrNamePars(std::string& name, Variables& variables)
+{
+	size_t pos = name.find('[');
+	if (pos == std::string::npos) {
+		throw std::invalid_argument("The [ symbol is expected.");
+	}
+	std::string name1 = name.substr(0, pos);
+	std::string val = name.substr(pos + 1, name.size() - pos - 2);
+	size_t i{};
+	if (variables.integers.contains(val)) {
+		i = variables.integers[val].first;
+	} else {
+		i = std::stoi(val);
+	}
+	std::pair<std::string, int> arrInfo(name1, i);
+	return arrInfo;
 }
 
 #endif //PARSER_H
--- variables.h
+++ variables.h
@@ -11,6 +11,11 @@ struct Variables
 	std::map<std::string, std::pair<char, std::string>> characters;
 	std::map<std::string, std::pair<int, std::string>> integers;
 	std::map<std::string, std::pair<std::string, std::string>> strings;
+	std::map<std::string, std::vector<bool>> boolArr;
+	std::map<std::string, std::vector<char>> charArr;
+	std::map<std::string, std::vector<int>> intArr;
+	std::map<std::string, std::vector<std::string>> stringArr;
+	
 };
 
 /*
